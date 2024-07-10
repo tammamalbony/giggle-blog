ce8a27f8b6cd773c311c3f3ffd49939810623510
tammamalbony <tammamalbony26@gmail.com>
2024-07-10 02:05:30 -0700

    add user model and add .gitignore

---
+++ protected/views/user/_form.php
@@ -0,0 +1,64 @@
+<?php
+/* @var $this UserController */
+/* @var $model User */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'user-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'email'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'password'); ?>
+		<?php echo $form->passwordField($model,'password',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'password'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'verification_token'); ?>
+		<?php echo $form->textField($model,'verification_token',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'verification_token'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'is_verified'); ?>
+		<?php echo $form->textField($model,'is_verified'); ?>
+		<?php echo $form->error($model,'is_verified'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'created_at'); ?>
+		<?php echo $form->textField($model,'created_at'); ?>
+		<?php echo $form->error($model,'created_at'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'updated_at'); ?>
+		<?php echo $form->textField($model,'updated_at'); ?>
+		<?php echo $form->error($model,'updated_at'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
---
+++ protected/views/user/_view.php
@@ -0,0 +1,37 @@
+<?php
+/* @var $this UserController */
+/* @var $data User */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('email')); ?>:</b>
+	<?php echo CHtml::encode($data->email); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('password')); ?>:</b>
+	<?php echo CHtml::encode($data->password); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('verification_token')); ?>:</b>
+	<?php echo CHtml::encode($data->verification_token); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('is_verified')); ?>:</b>
+	<?php echo CHtml::encode($data->is_verified); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('created_at')); ?>:</b>
+	<?php echo CHtml::encode($data->created_at); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('updated_at')); ?>:</b>
+	<?php echo CHtml::encode($data->updated_at); ?>
+	<br />
+
+
+</div>
